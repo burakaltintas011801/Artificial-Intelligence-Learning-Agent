caecd5311c15bb6f7d9530a45e9932382a78239f
burakaltintas011801 <72281243+burakaltintas011801@users.noreply.github.com>
2021-11-24 22:14:40 +0300

    The agent that can negotiate with other agents and learn from them  has been  implemented

---
+++ ANL-2021-example-agent-master/src/main/java/geniusweb/exampleparties/learningagent/deneme.java
@@ -0,0 +1,12 @@
+package geniusweb.exampleparties.learningagent;
+
+public class deneme {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("Bye bye");
+        System.out.println("hello everybody");
+        System.out.println("My name is nurettin");
+        System.out.println("My name is Can");
+
+    }
+}
